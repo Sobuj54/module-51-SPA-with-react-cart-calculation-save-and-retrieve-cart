a283ce7e0d116209208b9190ee9d03537084db63
soubj <sobujahmed124@gmail.com>
2023-04-19 22:38:57 +0600

    added onClick event handler function

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,16 +3,19 @@ import './Product.css';
 
 const Product = (props) => {
     const {name,seller,price,ratings,img} = props.product;
+    const handleAddToCart = props.handleAddToCart;
+
     return (
         <div className='product'>
             <img src={img} alt="" />
             <div className='product-info'>
-            <h6 className='product-name'>{name}</h6>
-            <p>Price : ${price}</p>
-            <p>Manufacturer : {seller}</p>
-            <p>Ratings : {ratings} stars</p>
+                <h6 className='product-name'>{name}</h6>
+                <p>Price : ${price}</p>
+                <p>Manufacturer : {seller}</p>
+                <p>Ratings : {ratings} stars</p>
             </div>
-            <button className='btn-cart'>
+
+            <button className='btn-cart' onClick={()=>handleAddToCart(props.product)}>
                 Add to Cart
             </button>
         </div>
