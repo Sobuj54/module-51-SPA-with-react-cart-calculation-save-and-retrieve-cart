3f535c8f0f7fd8db2fba8a5fb9b7975f88f60298
soubj <sobujahmed124@gmail.com>
2023-04-19 21:30:26 +0600

    added product info and added add to cart button position

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -6,6 +6,15 @@ const Product = (props) => {
     return (
         <div className='product'>
             <img src={img} alt="" />
+            <div className='product-info'>
+            <h6 className='product-name'>{name}</h6>
+            <p>Price : ${price}</p>
+            <p>Manufacturer : {seller}</p>
+            <p>Ratings : {ratings} stars</p>
+            </div>
+            <button className='btn-cart'>
+                Add to Cart
+            </button>
         </div>
     );
 };
