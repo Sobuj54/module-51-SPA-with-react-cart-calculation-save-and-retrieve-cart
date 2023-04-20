1e6534279df9083e4a47b4f6e75b7b95300de114
soubj <sobujahmed124@gmail.com>
2023-04-20 16:29:19 +0600

    easy to properly add porduct quantity

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,16 +6,27 @@ const Cart = (props) => {
 
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
+
     for(const product of cart){
-        totalPrice = totalPrice + product.price;
-        totalShipping = totalShipping + product.shipping;
+        // way 1
+        if(product.quantity == 0){
+            product.quantity = 1;
+        }
+        // way 2
+        // simple way
+        //product.quantity = product.quantity || 1;
+
+        totalPrice = totalPrice + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping * product.quantity;
+        quantity = quantity + product.quantity;
     }
     const tax = totalPrice * 7 /100;
     const grandTotal = totalPrice + totalShipping + tax;
     return (
         <div className='cart'>
              <h3>order summary </h3>
-            <h4>selected items : {cart.length}</h4>
+            <h4>selected items : {quantity}</h4>
             <p>Total Price : ${totalPrice}</p>
             <p>Total Shipping Charge : ${totalShipping}</p>
             <p>Tax : ${tax.toFixed(2)}</p>
